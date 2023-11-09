7a5b33e000da58c8f864fae2c948fc0d5e7b9a9a
QleanGG <guyguz1@gmail.com>
2023-11-10 00:46:09 +0200

    add new animation to counters and new event listener with the animations

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,19 +12,42 @@ const observer = new IntersectionObserver ((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// 
-const changeProjectNum = () => {
-    projectNum = document.getElementById("project-num");
-    projectNum.innerHTML = "250";
+function animateCount(element, current, target, duration) {
+    let start = current;
+    const step = target / (duration / 16); // 16ms for smooth animation
+
+    function updateCount() {
+        start += step;
+        element.textContent = Math.floor(start);
+
+        if (start < target) {
+            requestAnimationFrame(updateCount);
+        } else {
+            element.textContent = target;
+        }
+    }
+
+    requestAnimationFrame(updateCount);
 }
 
-const changeExpNum = () => {
-    expNum = document.getElementById("exp-num");
-    expNum.innerHTML = "12";
-}
+// Get the elements and target numbers
+const expElement = document.querySelector('#exp-num');
+const projectElement = document.querySelector('#project-num');
+let expCount = 1; // years of experience
+let projectCount = 2; // projects
 
-changeBtn = document.getElementById("change-btn");
-changeBtn.addEventListener('click',() => {
-    changeProjectNum();
-    changeExpNum();
-})
+window.addEventListener('load', function() {
+    animateCount(expElement, 0,  expCount, 800); // 1000ms (1 seconds) animation duration
+    animateCount(projectElement, 0, projectCount, 800);
+});
+
+// Update button event listener
+document.getElementById('change-btn').addEventListener('click', function() {
+    // Set new target numbers
+    const newExpTarget = 12;
+    const newProjectTarget = 250;
+
+    // Animate the numbers from their current values to the new targets
+    animateCount(expElement, expCount, newExpTarget, 500); // 500ms animation duration
+    animateCount(projectElement, projectCount, newProjectTarget, 500);
+});
